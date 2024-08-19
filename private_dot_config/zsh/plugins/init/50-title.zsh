ba6ea03f02f7e4c77133b70297f85dd46eadc689
Atif Chowdhury <iftakhar.awal@gmail.com>
2024-08-19 12:05:02 +0600

    add new files

---
+++ private_dot_config/zsh/plugins/init/50-title.zsh
@@ -0,0 +1,83 @@
+# based on ohmyzsh/lib/termsupport.zsh
+
+function title() {
+  # Don't set the title if inside emacs, unless using vterm
+  [[ -n "${INSIDE_EMACS:-}" && "$INSIDE_EMACS" != vterm ]] && return
+  
+  # if $2 is unset use $1 as default
+  # if it is set and empty, leave it as is
+  : ${2=$1}
+
+  case "$TERM" in
+    cygwin|xterm*|putty*|rxvt*|konsole*|ansi|mlterm*|alacritty|st*|wezterm*)
+      print -Pn "\e]2;${2:q}\a"
+      print -Pn "\e]1;${1:q}\a"
+      ;;
+    screen*|tmux*)
+      print -Pn "\ek${1:q}\e\\"
+      ;;
+  esac
+}
+
+function __title_precmd() {
+  title "%n@%m:%30<…<%~%<<"
+}
+
+function __title_preexec() {
+  emulate -L zsh -o extended_glob
+
+  # split command into array of arguments
+  local -a cmdargs
+  cmdargs=("${(z)2}")
+  # if running fg, extract the command from the job description
+  if [[ "${cmdargs[1]}" = fg ]]; then
+    # get the job id from the first argument passed to the fg command
+    local job_id jobspec="${cmdargs[2]#%}"
+    # logic based on jobs arguments:
+    # http://zsh.sourceforge.net/Doc/Release/Jobs-_0026-Signals.html#Jobs
+    # https://www.zsh.org/mla/users/2007/msg00704.html
+    case "$jobspec" in
+      <->) # %number argument:
+        # use the same <number> passed as an argument
+        job_id=${jobspec} ;;
+      ""|%|+) # empty, %% or %+ argument:
+        # use the current job, which appears with a + in $jobstates:
+        # suspended:+:5071=suspended (tty output)
+        job_id=${(k)jobstates[(r)*:+:*]} ;;
+      -) # %- argument:
+        # use the previous job, which appears with a - in $jobstates:
+        # suspended:-:6493=suspended (signal)
+        job_id=${(k)jobstates[(r)*:-:*]} ;;
+      [?]*) # %?string argument:
+        # use $jobtexts to match for a job whose command *contains* <string>
+        job_id=${(k)jobtexts[(r)*${(Q)jobspec}*]} ;;
+      *) # %string argument:
+        # use $jobtexts to match for a job whose command *starts with* <string>
+        job_id=${(k)jobtexts[(r)${(Q)jobspec}*]} ;;
+    esac
+
+    # override preexec function arguments with job command
+    if [[ -n "${jobtexts[$job_id]}" ]]; then
+      1="${jobtexts[$job_id]}"
+      2="${jobtexts[$job_id]}"
+    fi
+  fi
+
+  # cmd name only, or if this is sudo or ssh, the next cmd
+  local CMD="${1[(wr)^(*=*|sudo|ssh|mosh|rake|-*)]:gs/%/%%}"
+  local LINE="${2:gs/%/%%}"
+
+  title "$CMD" "%50>...>${LINE}%<<"
+}
+
+# emits the control sequence to notify the terminal of cwd
+function __title_cwd() {
+  print -Pn "\e]7;file://%m%d\e\\"
+}
+
+if [[ -z "$INSIDE_EMACS" || "$INSIDE_EMACS" = vterm ]]; then
+  add-zsh-hook -Uz precmd __title_precmd
+  add-zsh-hook -Uz preexec __title_preexec
+fi
+
+add-zsh-hook -Uz precmd __title_cwd
