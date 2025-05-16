02dd83f497a54b93d4bffb1c5988be65ab64c92f
Atif Chowdhury <iftakhar.awal@gmail.com>
2025-05-17 00:09:51 +0600

    Add .config/nvim/lua/plugins/colorscheme.lua

---
+++ private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,23 @@
+return {
+  -- { "olimorris/onedarkpro.nvim" },
+  -- {
+  --   "navarasu/onedark.nvim",
+  --   config = function()
+  --     require("onedark").setup({
+  --       style = "darker",
+  --     })
+  --
+  --     require("onedark").load()
+  --   end,
+  -- },
+  {
+    "folke/tokyonight.nvim",
+    opts = { style = "night" },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      -- colorscheme = "tokyonight",
+    },
+  },
+}
