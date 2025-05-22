399929aeb01275bf21d9d54b442463830cea1bca
Atif Chowdhury <iftakhar.awal@gmail.com>
2025-05-22 17:32:28 +0600

    Update .config/nvim/lazyvim.json Update .config/nvim/lua/plugins/lualine.lua Update .config/zsh/conf.d/compstyle.zsh Update .config/zsh/.zshenv Update .config/zsh/.zshrc

--- private_dot_config/nvim/lua/plugins/lualine.lua
+++ private_dot_config/nvim/lua/plugins/lualine.lua
@@ -3,17 +3,12 @@ return {
   opts = function(_, opts)
     table.insert(opts.sections.lualine_x, {
       "lsp_status",
-      icon = "", -- f013
+      icon = "",
       symbols = {
-        -- Standard unicode symbols to cycle through for LSP progress:
-        -- spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" },
         spinner = "",
-        -- Standard unicode symbol for when LSP is done:
         done = "",
-        -- Delimiter inserted between LSP names:
         separator = " ",
       },
-      -- List of LSP names to ignore (e.g., `null-ls`):
       ignore_lsp = { "copilot" },
     })
     -- opts.sections.lualine_z = {
