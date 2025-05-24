737ed65a0abdbdab934238ba231dfee4a1df92fd
Atif Chowdhury <iftakhar.awal@gmail.com>
2025-05-24 23:34:17 +0600

    Update .config/zsh/.zplugins Update .config/zsh/.zshrc Update .config/zsh/plugins/xdg.zsh

--- private_dot_config/zsh/plugins/xdg.zsh
+++ private_dot_config/zsh/plugins/xdg.zsh
@@ -40,21 +40,20 @@ export SQLITE_HISTORY="${SQLITE_HISTORY:-$XDG_DATA_HOME/sqlite/history}"
 [[ ! -d ${SQLITE_HISTORY:h} ]] && mkdir -p ${SQLITE_HISTORY:h}
 
 # git
-git_cfg=$XDG_CONFIG_HOME/git
-[[ ! -d $git_cfg ]] && mkdir -p $git_cfg
-for ext in git{config,ignore,attributes,-credentials,k}; do
-  [[ -f $ext && -s $ext ]] || return
-  src=$HOME/.${ext}
-  case $ext in
-    'gitk')
-      mv -b $src $git_cfg/gitk
-      ;;
-    'git-credentials')
-      mv -b $src $git_cfg/credentials
-      ;;
-    *)
-      mv -b $src $git_cfg/${ext//.git/}
-      ;;
-  esac
-done
-unset git_cfg
+() {
+  local git_cfg ext src
+  git_cfg=$XDG_CONFIG_HOME/git
+  [[ ! -d $git_cfg ]] && mkdir -p $git_cfg
+  for ext in git{config,ignore,attributes,-credentials,k}; do
+    src=$HOME/.${ext}
+    [[ -f $src ]] || continue
+    case $ext in
+      'gitk') mv -b $src $git_cfg/gitk ;;
+      'git-credentials') mv -b $src $git_cfg/credentials;;
+      *) mv -b $src $git_cfg/${ext//.git/} ;;
+    esac
+  done
+}
+
+# revolver
+export REVOLVER_DIR="${REVOLVER_DIR:-$XDG_DATA_HOME/revolver}"
